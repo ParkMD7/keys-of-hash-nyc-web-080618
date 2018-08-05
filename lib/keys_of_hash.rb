85421a3775180d470f0752bcd0199972603fa258
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-08-05 04:58:18 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 class Hash
   def keys_of(arguments)
-    # code goes here
+    
+  def keys_of(*arguments)
+    monkey_patch_array = []
+    
+    arguments.each do |arg|
+      #binding.pry
+        self.each do |arg_key, arg_value|
+          binding.pry
+        end
+    end
+  end  
   end
 end
